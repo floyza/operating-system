30df8d00eab60611fe678a9f6111da5b70e19727
Gavin Downard <gavin.downard@runbox.com>
2021-10-16 11:12:12 -0700

    Implement page handler, partly borked
    Currently, calling terminal.putCharAt results in a triple fault. An
    exception handler will be implemented before I continue to debug it.

---
+++ boot.s
@@ -0,0 +1,85 @@
+  .set ALIGN, 1 << 0
+  .set MEMINFO, 1 << 1
+  .set MAGIC, 0x1BADB002
+  .set FLAGS, ALIGN | MEMINFO
+  .set CHECKSUM, -(MAGIC + FLAGS)
+
+  .section .multiboot.data, "aw"
+  .align 4
+  .long MAGIC
+  .long FLAGS
+  .long CHECKSUM
+
+  .section .bootstrap_stack, "aw", @nobits
+stack_bottom:
+  .skip 16384 // 16 KiB
+stack_top:
+
+  .section .bss, "aw", @nobits
+  .align 4096
+boot_page_directory:
+  .skip 4096
+boot_page_table1:
+  .skip 4096
+  // we only keep a single page table because our kernel memory usage <= 3 MiB
+  // and each page holds 4 MiB
+
+  .section .multiboot.text, "a"
+  .global _start
+  .type _start, @function
+_start:
+  // Page the first 1023 double words (16 bit), setting them both present, readable and writable
+  // FIXME: .rodata and .text should NOT be writable
+  //movl $(0x100000 | 3), %eax
+  movl $0, %eax
+  movl $1023, %ecx
+  movl $(boot_page_table1 - 0xC0000000), %edi
+.nextPage:
+  // should be able to use stosd instead?
+  cmpl $_kernel_start, %eax
+  jl .skipWrite
+  cmpl $(_kernel_end - 0xC0000000), %eax
+  jge .donePaging
+  movl %eax, %edx
+  orl $3, %edx
+  movl %edx, (%edi)
+.skipWrite:
+  addl $4, %edi
+  addl $0x1000, %eax
+  loop .nextPage
+.donePaging:
+
+  // Page VGA in the last remaining page, from 0x000B8000 to 0xC03FF000, as present and r/w
+  movl $(0xB8000 | 3), boot_page_table1 - 0xC0000000 + 1023 * 4
+
+  // Identity mapping
+  movl $(boot_page_table1 - 0xC0000000 + 3), boot_page_directory - 0xC0000000 + 0
+
+  // Map onto 0xC0000000
+  movl $(boot_page_table1 - 0xC0000000 + 3), boot_page_directory - 0xC0000000 + 768*4
+
+  // Enable paging, set protection bit, and set read-only enforcement for supervisor
+  movl $(boot_page_directory - 0xC0000000), %ecx
+  movl %ecx, %cr3
+  movl %cr0, %eax
+  orl $0x80010001, %eax
+  movl %eax, %cr0
+
+  lea paged, %ecx
+  jmp *%ecx
+
+.section .text
+paged:
+  // Remove identity mapping
+  movl $0, boot_page_directory + 0
+
+  // Reload cr3 to force a TLB flush to reload page directory
+  movl %cr3, %eax
+  movl %eax, %cr3
+
+  movl $stack_top, %esp // setup stack
+
+  call kmain
+
+  cli
+  hlt
--- build.zig
+++ build.zig
@@ -12,6 +12,7 @@ pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
 
     const kernel = b.addExecutable("os", "kernel.zig");
+    kernel.addAssemblyFile("boot.s");
     kernel.setTarget(target);
     kernel.setBuildMode(mode);
     kernel.setLinkerScriptPath("linker.ld");
@@ -19,29 +20,37 @@ pub fn build(b: *Builder) void {
     kernel.install();
 
     const common_params = [_][]const u8{ "qemu-system-i386", "-kernel", kernel.getOutputPath() };
+    const verbose_params = [_][]const u8{ "-d", "int,cpu_reset" };
     const debug_params = [_][]const u8{ "-s", "-S" };
 
     var qemu_params = std.ArrayList([]const u8).init(b.allocator);
+    var qemu_verbose_params = std.ArrayList([]const u8).init(b.allocator);
     var qemu_debug_params = std.ArrayList([]const u8).init(b.allocator);
     for (common_params) |str| {
         qemu_params.append(str) catch unreachable;
+        qemu_verbose_params.append(str) catch unreachable;
         qemu_debug_params.append(str) catch unreachable;
+    }
+    for (verbose_params) |str| {
+        qemu_verbose_params.append(str) catch unreachable;
     }
     for (debug_params) |str| {
         qemu_debug_params.append(str) catch unreachable;
     }
 
     const qemu = b.step("qemu", "Run the OS in qemu");
+    const qemu_verbose = b.step("qemu-verbose", "Run the OS in qemu, set to output more log messages");
     const qemu_debug = b.step("qemu-debug", "Run the OS in qemu, waiting for a debugger to attach");
 
     const run_qemu = b.addSystemCommand(qemu_params.items);
+    const run_qemu_verbose = b.addSystemCommand(qemu_verbose_params.items);
     const run_qemu_debug = b.addSystemCommand(qemu_debug_params.items);
 
     run_qemu.step.dependOn(b.getInstallStep());
+    run_qemu_verbose.step.dependOn(b.getInstallStep());
     run_qemu_debug.step.dependOn(b.getInstallStep());
-    // run_qemu.step.dependOn(&kernel.step);
-    // run_qemu_debug.step.dependOn(&kernel.step);
 
     qemu.dependOn(&run_qemu.step);
+    qemu_verbose.dependOn(&run_qemu_verbose.step);
     qemu_debug.dependOn(&run_qemu_debug.step);
 }
