5a14fdef9bfefb6281020c3f3fe666627f083025
Gavin Downard <gavin.downard@runbox.com>
2021-10-26 12:03:10 -0700

    Add general logging capabilities with printk

--- kernel.zig
+++ kernel.zig
@@ -4,44 +4,30 @@ const terminal = @import("terminal.zig");
 const serial = @import("serial.zig");
 const gdt = @import("gdt.zig");
 const idt = @import("idt.zig");
+const log = @import("log.zig");
 
 pub fn panic(msg: []const u8, error_return_trace: ?*builtin.StackTrace) noreturn {
     @setCold(true);
-    serial.put('~');
-    // terminal.write("KERNEL PANIC: ");
-    // terminal.write(msg);
+    serial.write("KERNEL PANIC: ");
+    serial.write(msg);
+    terminal.write("KERNEL PANIC: ");
+    terminal.write(msg);
     while (true) {}
 }
 
-fn print(buf: []u8, comptime fmt: []const u8, args: anytype) void {
-    var x: []u8 = std.fmt.bufPrint(&stack_bytes, fmt, args) catch |_| {
-        panic("print() buffer ran out of memory", null);
-    };
-    terminal.writeLn(fmt);
-}
-
-export fn kmain() void {
+export fn kmain() noreturn {
     gdt.initialize();
     idt.initialize();
 
-    // terminal.initialize();
-    // terminal.writeLn("Kernel started");
+    terminal.initialize();
 
     serial.initialize() catch |err| switch (err) {
         error.MissingPort => @panic("No serial port"),
         error.FaultyPort => @panic("Serial port is faulty"),
     };
 
-    serial.put('H');
-    serial.put('i');
-    serial.put('\r');
-    serial.put('\n');
-    serial.put('w');
-    serial.put('r');
-    serial.put('d');
-    serial.put('!');
-
-    // terminal.writeLn("Serial port written");
+    log.printk_ln("Hello, {s}", .{"world!"});
+    log.printk_ln("Hello, second world!", .{});
 
     while (true) {}
 }
---
+++ log.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const terminal = @import("terminal.zig");
+const serial = @import("serial.zig");
+
+pub fn printk(comptime fmt: []const u8, args: anytype) void {
+    var buf: [100]u8 = undefined;
+    const str: []u8 = std.fmt.bufPrint(&buf, fmt, args) catch |_| {
+        @panic("print() buffer ran out of memory");
+    };
+    terminal.write(str);
+    serial.write(str);
+}
+
+pub fn printk_ln(comptime fmt: []const u8, args: anytype) void {
+    var buf: [100]u8 = undefined;
+    const str: []u8 = std.fmt.bufPrint(&buf, fmt, args) catch |_| {
+        @panic("print() buffer ran out of memory");
+    };
+    terminal.writeLn(str);
+    serial.write_ln(str);
+}
